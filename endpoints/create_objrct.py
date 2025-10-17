748f9bf696793111fcbc4dcb004a9a9ce3cb6fe6
sem1on <sgorlov21@gmail.com>
2025-10-17 17:55:48 +0300

    add check methods

--- endpoints/create_objrct.py
+++ endpoints/create_objrct.py
@@ -1,9 +1,16 @@
+import requests
+
 class CreateObject:
 
     response = None
     response_json = None
 
     def new_object(self, payload):
-        response = None
-        response_json = None
+        self.response = requests.post(
+            url="https://api.restful-api.dev/objects",
+            json=payload
+        )
+        self.response_json = self.response.json()
 
+    def chek_name(self, name):
+        assert self.response_json["name"] == name
